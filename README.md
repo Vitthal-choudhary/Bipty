37ce0c62bd67ceee1951378e212b2c7518050204
Vitthal-choudhary <iamvitthal1412@gmail.com>
2024-03-13 12:54:54 +0530

    Project

---
+++ README.md
@@ -0,0 +1,14 @@
+# Stork
+A Simple ChatBot webapp - "Stork"
+
+This webapp uses chatengine.io API, Python, React js for diffrent purposes
+
+To use thisyou need to 
+
+1. Open Terminal in server directory and type following commands
+       venv\Scripts\activate.bat
+       uvicorn main:app --reload --port 3001
+2. Next, open terminal in client directory and type
+       npm run start
+
+Thank You
---
+++ client/src/chatsPage.js
@@ -0,0 +1,16 @@
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <PrettyChatWindow
+        projectId="08427bfc-2076-4ae1-98f8-4fa7b975b708"
+        username={props.user.username} // adam
+        secret={props.user.secret} // pass1234
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
